383ff9468afd5e118bc42daebec1c5f784ffa42e
Carlotta <juan.rubio262132@potros.itson.edu.mx>
2026-03-13 09:07:39 -0700

    Ajustes en signup.js

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,14 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')
-
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+
+    name = data.get("name")
+    email = data.get("email")
+    password = data.get("password")
+
+    print(f"Nombre: {name}")
+    print(f"Email: {email}")
+
+    return jsonify({'success': True})
 #underscorte methods and properties
 if __name__ == '__main__':
     app.run()
--- static/js/dogo/signup.js
+++ static/js/dogo/signup.js
@@ -1,8 +1,8 @@
-document.getElementById('btn-register').addEventListener("click", register());
+document.getElementById('btn-register').addEventListener("click", register);
 
 function register(){
-    password = document.getElementById('user-password').value
-    repeat_password = document.getElementById('user-repeat-password').value
+    var password = document.getElementById('user-password').value
+    var repeat_password = document.getElementById('user-confirm-password').value
     
     if(password != repeat_password){
         alert("Las contraseñas no coinciden")
@@ -14,7 +14,26 @@ function register(){
     const data = {
         name: document.getElementById('user-name').value,
         email: document.getElementById('user-email').value,
-        password: document.getElementById('user-password').value,
-        
-    }
+        password: document.getElementById('user-password').value,   
+    };
+
+            //endpoint
+    fetch('api/users', {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+    .then(result => {
+        if(result.success){
+            alert("Usuario guardo correctamente")
+        }else{
+            alert("Error al guardar el usuario")
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+
+
 }
